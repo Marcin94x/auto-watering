fb38ea6da8c7c026ad90fabb73c85a2b2d9d48b5
Marcin W <iim1@wp.pl>
2016-04-23 23:38:43 +0200

    Server TCP

--- server.ino
+++ server.ino
@@ -1,57 +1,113 @@
 #include <ESP8266.h>
 
-
 const char SSID[] = "castle";
 const char PASS[] = "zamek12345678";
-const char HOST_NAME[] = "www.baidu.com";
-const byte HOST_PORT = 80;
 
-String plainRequest = "";
-
+uint32_t len;
+uint8_t buffer[128];
+uint8_t mux_id;
+String request;
 
 void setupServer() {
-  if (client.setOprToStationSoftAP());
-  else
-  {
-    Serial.println("softap error!");
+  Serial.println("setup wifi begin");
+  
+  Serial.print("FW Version:");
+  Serial.println(wifi.getVersion().c_str());
+  
+  if (wifi.setOprToStationSoftAP()) {
+    Serial.println("to station + softap ok");
+  } else {
+    Serial.println("to station + softap err");
   }
-  if (client.joinAP(SSID, PASS))
-  {
-    Serial.print("IP:");
-    Serial.println(client.getLocalIP().c_str());
+  
+  if (wifi.joinAP(SSID, PASS)) {
+    Serial.println("Join AP success");
+    Serial.print("IP: ");
+    Serial.println(wifi.getLocalIP().c_str());    
+  } else {
+    Serial.println("Join AP failure");
   }
-  else
-  {
-    Serial.println("join AP error!");
+  
+  if (wifi.enableMUX()) {
+    Serial.println("multiple ok");
+  } else {
+    Serial.println("multiple err");
   }
-  if (client.disableMUX());
-  else
-  {
-    Serial.println("single error!");
+  
+  if (wifi.startTCPServer(8090)) {
+    Serial.println("start tcp server ok");
+  } else {
+    Serial.println("start tcp server err");
   }
-  if (client.createTCP(HOST_NAME, HOST_PORT));
-  else
-  {
-    Serial.println("create tcp err!");
+  
+  if (wifi.setTCPServerTimeout(10)) { 
+    Serial.println("set tcp server timout 10 seconds");
+  } else {
+    Serial.println("set tcp server timout err");
   }
+
+  Serial.println("setup end");
 }
 
 void listenClients()
 {
-  
-  char *hello = "GET / HTTP/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n";
-  uint8_t buffer[1024] = {0};
-  client.send((const uint8_t*)hello, strlen(hello));
-  uint32_t len =client.recv(buffer, sizeof(buffer), 10000);
+  len = wifi.recv(&mux_id, buffer, sizeof(buffer), 100);
   if (len > 0) {
-    Serial.print("Received:[");
+    Serial.print("Status:[");
+    Serial.print(wifi.getIPStatus().c_str());
+    Serial.println("]");
+
+    Serial.print("Received from :");
+    Serial.print(mux_id);
+    Serial.print("[");
     for(uint32_t i = 0; i < len; i++) {
-      Serial.print((char)buffer[i]);
-     }
-     Serial.print("]\r\n");
+      Serial.print((char) buffer[i]);
+    }
+    Serial.println("]");
+    parseRequest();
+
+    /*if(wifi.send(mux_id, buffer, len)) {
+      Serial.print("send back ok\r\n");
+    } 
+    else {
+      Serial.print("send back err\r\n");
+    }*/
+
+    if (wifi.releaseTCP(mux_id)) {
+      Serial.print("release tcp ");
+      Serial.print(mux_id);
+      Serial.println(" ok");
+    } else {
+      Serial.print("release tcp");
+      Serial.print(mux_id);
+      Serial.println(" err");
+    }
+
+    Serial.print("Status:[");
+    Serial.print(wifi.getIPStatus().c_str());
+    Serial.println("]");
   }
 }
 
 void parseRequest()
 {
+  request = "";
+  for (uint32_t i = 0; i < len - 1; i++) {
+    request += (char) buffer[i];
+  }
+  if (request == "gettemp") {
+    getTemperature();
+  }
+  if (request == "getairhumid") {
+    getAirHumidity();
+  }
+  if (request == "getsoilhumid") {
+    getSoilHumidity();
+  }
+  if (request == "getinsol") {
+    getInsolation();
+  }
+  if (request == "getliquidlvl") {
+    getLiquidLevel();
+  }
 }
--- watering.ino
+++ watering.ino
@@ -9,19 +9,19 @@ const byte DHT_PIN = 9;
 const byte SOIL_PIN = 10;
 const byte PUMP_PIN = 11;
 
-ESP8266 client(Serial1);
+ESP8266 wifi(Serial3, 115200);
 DHT dhtSensor(DHT_PIN, DHT11);
 Adafruit_TSL2561_Unified tsl_sensor = Adafruit_TSL2561_Unified(TSL2561_ADDR_FLOAT, 12345);
-bool is_pump_on = false;
 
-char buffer[10];
+extern uint8_t mux_id;
+bool is_pump_on = false;
 unsigned int start_time = 0; // start time of pump
 unsigned int current_time = 0;
 const unsigned long line_time = 30 * 1000;
 
 void setup() {
   Serial.begin(9600);
-  //setupServer();
+  setupServer();
   setupSensors();
   setupDebugSensors();
   pinMode(PUMP_PIN, OUTPUT);
@@ -29,44 +29,50 @@ void setup() {
 }
 
 void loop() {
-  //listenClients();
-  parseRequest();
+  listenClients();
   debugSensors();
   watchdog();
 }
 
 int getTemperature() {
-  sprintf(buffer, "%d", dhtSensor.readTemperature());
+  char buffer[10];
+  sprintf(buffer, "t:%d", 33);
+  Serial.print("buffer: ");
+  Serial.println(buffer);
   const uint8_t *temp = (const uint8_t *) buffer;
-  return client.send(temp, strlen(buffer));
+  return wifi.send(mux_id, temp, strlen(buffer));
 }
 
 int getAirHumidity() {
+  char buffer[10];
   sprintf(buffer, "%d", dhtSensor.readHumidity());
   const uint8_t *humid = (const uint8_t *) buffer;
-  return client.send(humid, strlen(buffer));
+  return wifi.send(mux_id, humid, strlen(buffer));
 }
 
 int getSoilHumidity() {
+  char buffer[10];
   int humidity = analogRead(SOIL_PIN) / 1024.0 * 100.0;
   sprintf(buffer, "%d", humidity);
   const uint8_t *humid = (const uint8_t *) buffer;
-  return client.send(humid, strlen(buffer));
+  return wifi.send(mux_id, humid, strlen(buffer));
 }
 
 int getInsolation() {
+  char buffer[10];
   sensors_event_t event;
   tsl_sensor.getEvent(&event);
   sprintf(buffer, "%d", event.light);
   const uint8_t *insol = (const uint8_t *) buffer;
-  return client.send(insol, strlen(buffer));
+  return wifi.send(mux_id, insol, strlen(buffer));
 }
 
 int getLiquidLevel() {
+  char buffer[10];
   int liquid_level = analogRead(LEVEL_PIN) / 1024.0 * 100.0;
   sprintf(buffer, "%d", liquid_level);
   const uint8_t *lev = (const uint8_t *) buffer;
-  return client.send(lev, strlen(buffer));
+  return wifi.send(mux_id, lev, strlen(buffer));
 }
 
 int setPump() {
